d46aa3cdaef2decc5eaf2eb0b4f206779344be43
Kwamz3 <charlie18appiah@gmail.com>
2026-09-11 12:25:33 +0000

    Addition of comapany favicon

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Navbar from "../components/Navbar.jsx"
-import Footer from "../components/Footer.jsx"
-import HeroSection from "../components/HeroSection.jsx"
+import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
+import HeroSection from "../components/HeroSection.jsx";
+import ServiceCard from "../components/ServiceCard.jsx";
+import Counter from "../components/common/Counter.jsx";
+import "../styles/utils.css"
+import "../styles/categories.css"
+
+const Homepage = () => {
+    return (
+        <div className="page">
+            <Navbar />
+
+            <main className="page-content" style={{ maxWidth: '1440px', paddingBottom: '0' }}>
+                <HeroSection />
+
+                <section style={{ padding: "80px 20px", textAlign: "center" }}>
+
+                    <h2 style={{ fontSize: "2.5rem", marginBottom: "12px", color: "var(--color-text-main)" }}>
+                        How It <span style={{ color: "var(--color-gold)" }}>Works</span>
+                    </h2>
+                </section>
+            </main>
+        </div>
+    );
+};
