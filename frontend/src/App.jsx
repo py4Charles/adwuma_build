d11c5a8727bdc055885966a61f299d42974e2ac0
Kwamz3 <charlie18appiah@gmail.com>
2026-09-11 11:58:03 +0000

    App fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,2 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./styles/global.css"
+
+import ArtisanCard from "./components/ArtisanCard"
+import Homepage from "./pages/Homepage.jsx"
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/card" element={<ArtisanCard />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default App;
