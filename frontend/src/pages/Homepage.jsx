aac1cec2d9f138dd403b83486fc0f6fc7ce70360
Kwamz3 <charlie18appiah@gmail.com>
2026-09-11 04:30:49 +0000

    Add HeroSection component and implement Counter component for animated counting

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -2,3 +2,4 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar.jsx"
 import Footer from "../components/Footer.jsx"
+import HeroSection from "../components/HeroSection.jsx"
