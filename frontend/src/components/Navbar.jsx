47ae04fdfa8f9a86d3d6eb4df182b1bff43b91ef
Kwamz3 <charlie18appiah@gmail.com>
2026-09-18 06:57:12 +0000

    style: refactor Navbar layout and update styles for improved responsiveness and aesthetics

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,21 +4,24 @@ import logo from "../assets/icon.png"
 const Navbar = () => {
     return (
         <header className="navbar">
-            <Link to="/" className="navbar-brand">
-                <img src={logo} alt="Adwuma Logo" className="navbar-logo" />
-                <span className="navbar-brand-text">Adwuma</span>
-            </Link>
+            <div className="navbar-inner">
+                <Link to="/" className="navbar-brand">
+                    <img src={logo} alt="Adwuma Logo" className="navbar-logo" />
+                    {/* <span className="navbar-brand-text">Adwuma</span> */}
+                </Link>
 
-            <nav className="navbar-links">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-            </nav>
+                <nav className="navbar-links">
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+                </nav>
 
-            <div className="navbar-auth">
-                <Link to="/login" className="login-btn">Login</Link>
-                <Link to="/signup" className="signup-btn">Sign Up</Link>
+                <div className="navbar-auth">
+                    <Link to="/login" className="login-btn">Login</Link>
+                    <Link to="/signup" className="signup-btn">Sign Up</Link>
+                </div>
             </div>
+
         </header>
     );
 };
